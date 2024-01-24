e26b1765ee730db198e632d99da0700aad8ddd9b
Gabriel Martins <gabtec@sapo.pt>
2024-01-24 09:29:52 +0000

    update ver

--- version/version.go
+++ version/version.go
@@ -1,8 +1,8 @@
 package version
 
 var (
-	Version     string = "0.1.0"
-	GitSHA      string = "762d7d3539e0d22bc20cf55ffe1bb995cdeac774"
+	Version     string = "0.3.0"
+	GitSHA      string = "9e8e9e3cd601fb2d4f21efcd25d4b3fddff716eb"
 	GitRepo     string = "demo-ago-api"
 	GitProvider string = "github.com"
 )
