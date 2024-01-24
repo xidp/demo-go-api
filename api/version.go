7bd054dd1f5505a47481246c439bfc057ea3206c
Gabriel Martins <gabtec@sapo.pt>
2024-01-24 09:32:56 +0000

    feat: update version output

--- api/version.go
+++ api/version.go
@@ -9,7 +9,9 @@ import (
 
 func GetVersionHandler(e echo.Context) error {
 	return e.JSON(http.StatusOK, map[string]string{
-		"version": version.Version,
-		"gitSHA":  version.GitSHA,
+		"version":     version.Version,
+		"gitSHA":      version.GitSHA,
+		"gitRepo":     version.GitRepo,
+		"gitProvider": version.GitProvider,
 	})
 }
