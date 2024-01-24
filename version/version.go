be6ad0048541e152f852f1813dfaa21c907dbc26
Gabriel Martins <gabtec@sapo.pt>
2024-01-24 10:49:20 +0000

    chore: update version

--- version/version.go
+++ version/version.go
@@ -1,8 +1,8 @@
 package version
 
 var (
-	Version     string = "0.4.0"
-	GitSHA      string = "7bd054dd1f5505a47481246c439bfc057ea3206c"
+	Version     string = "0.6.0"
+	GitSHA      string = "b1d9a2f09b182c85275cf91761fabdd6331639e5"
 	GitRepo     string = "demo-ago-api"
 	GitProvider string = "github.com"
 )
